30722240165f705c1d333f9224835a5381375d88
KekysMaksimys <58911147+KekysMaksimys@users.noreply.github.com>
2023-07-17 17:22:00 +0300

    feat: done with catehory filters

--- src/components/select-filter.ts
+++ src/components/select-filter.ts
@@ -1,9 +1,11 @@
 import json from '../data.json';
 let selectCategoryArray = json;
+let indexesArray = Array.from(Array(json.length).keys());
 
 function selectCategory() {
     const selectCategoryAll = document.querySelectorAll('.select');
     selectCategoryArray = [];
+    indexesArray = Array.from(Array(json.length).keys());
     selectCategoryAll.forEach((el) => {
         json.forEach(
             (e: {
@@ -18,18 +20,39 @@ function selectCategory() {
                 category: string;
                 thumbnail: string;
                 images: string[];
-            }) => {
+            }, index) => {
                 if (e.category === el.id) {
                     selectCategoryArray.push(e);
+                    indexesArray.splice(index, 1);
                 }
             }
         );
     });
     if (selectCategoryAll.length === 0) {
         selectCategoryArray = json;
+        indexesArray = Array.from(Array(json.length).keys());
+        showAllCards();
+    } else{
+        showAllCards();
+        hideCards();
     }
-    console.log('selectCategoryArray: ', selectCategoryArray);
-    console.log('selectCategoryArray.length: ', selectCategoryArray.length);
+    console.log('indexes:',indexesArray)
 }
+
+function hideCards(){
+    const productCardsAll = document.querySelectorAll('.product-card');
+    indexesArray = [...new Set(indexesArray)];
+    indexesArray.forEach((el) =>{
+        productCardsAll[el].classList.add('hidden-card');
+    })
+}
+
+function showAllCards(){
+    const productCardsAll = document.querySelectorAll('.hidden-card');
+    productCardsAll.forEach((el) =>{
+        el.classList.remove('hidden-card');
+    })
+}
+
 
 export { selectCategory, selectCategoryArray };
